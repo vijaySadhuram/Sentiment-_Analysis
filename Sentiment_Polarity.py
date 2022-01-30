fb128135b38eb203205e18dd1101cb2437e0ac07
vijaySadhuram <98251620+vijaySadhuram@users.noreply.github.com>
2022-01-30 20:00:01 +0530

    Delete Sentiment_Polarity.py

--- Sentiment_Polarity.py
+++
@@ -1,7 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-sns.countplot(x=df['deceptive'],hue='polarity',data=df)
-sns.countplot(x=df['deceptive'],hue='source',data=df)
-#drop the feature hotel
-df=df.drop(['hotel'],axis=1)
-df
