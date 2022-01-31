275bfe5a4ad7ce21342645a9cd800bcfbc2b0662
vijaySadhuram <98251620+vijaySadhuram@users.noreply.github.com>
2022-01-31 19:36:02 +0530

    Update Sentiment_Polarity.py

--- Sentiment_Polarity.py
+++ Sentiment_Polarity.py
@@ -1,5 +1,7 @@
+#import seaborn
 import seaborn as sns
 import matplotlib.pyplot as plt
+#plot the graph
 sns.countplot(x=df['deceptive'],hue='polarity',data=df)
 sns.countplot(x=df['deceptive'],hue='source',data=df)
 #drop the feature hotel
